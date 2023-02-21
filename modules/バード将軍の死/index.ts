70185dedd894719d73222f68e695ef8b45203d10
himu <zero.256dic@gmail.com>
2023-02-21 14:35:21 +0900

    バード将軍の死 完成

--- modules/バード将軍の死/index.ts
+++ modules/バード将軍の死/index.ts
@@ -11,9 +11,11 @@ import {
   noPermission,
   readonlyPermission,
   sendInfoToIndividualChannel,
+  writerPermission,
 } from "../../src/util";
 import { readFileSync } from "node:fs";
 import { AScenario } from "../../src/module";
+import { config } from "../../src/config";
 
 const file = FILE(__dirname);
 
@@ -35,6 +37,11 @@ const scenes = [
   "延長投票",
   "投票",
   "エンディング",
+  "解説・ヘンリ二世",
+  "解説・ジャック王子",
+  "解説・騎士ハリス",
+  "解説・執事トム",
+  "解説・女中メアリ",
   "解説",
 ];
 
@@ -156,6 +163,9 @@ export class Scenario extends AScenario {
   }
 
   async scene(_scene: string): Promise<void> {
+    const audienceRole = await this.getRole("観戦");
+    const playersRole = await this.getRole("PL");
+
     if (_scene === "事前") {
       const txtHajimeni = readFileSync(file("0_はじめに.txt")).toString();
       const txtRule = readFileSync(file("1_ルール.txt")).toString().split("@");
@@ -215,11 +225,105 @@ export class Scenario extends AScenario {
         })
       );
       await sendInfoToIndividualChannel(chCharacters);
-      // } else if (_scene === "ルール説明") {
-      // } else if (_scene === "HO配布") {
-      // } else if (_scene === "プロローグ") {
-      // } else if (_scene === "エンディング") {
-      // } else if (_scene === "解説") {
+    } else if (_scene === "ルール説明") {
+      const chCommonInfo = await this.getTextChannel("共通情報");
+      await chCommonInfo.permissionOverwrites.set([
+        noPermission(this.guild.roles.everyone.id),
+        readonlyPermission(audienceRole.id),
+        readonlyPermission(playersRole.id),
+      ]);
+    } else if (_scene === "HO配布") {
+      const ho1 = readFileSync(file("HO1.txt")).toString();
+      const ho2 = readFileSync(file("HO2.txt")).toString();
+      const ho3 = readFileSync(file("HO3.txt")).toString();
+      const ho4 = readFileSync(file("HO4.txt")).toString();
+      const ho5 = readFileSync(file("HO5.txt")).toString();
+
+      const chHo1 = await this.getTextChannel("ヘンリ二世");
+      await chHo1.send({ content: ho1 });
+      const chHo2 = await this.getTextChannel("ジャック王子");
+      await chHo2.send({ content: ho2 });
+      const chHo3 = await this.getTextChannel("騎士ハリス");
+      await chHo3.send({ content: ho3 });
+      const chHo4 = await this.getTextChannel("執事トム");
+      await chHo4.send({ content: ho4 });
+      const chHo5 = await this.getTextChannel("女中メアリ");
+      await chHo5.send({ content: ho5 });
+    } else if (_scene === "プロローグ") {
+      const chPrologue = await this.getTextChannel("プロローグ");
+      await chPrologue.permissionOverwrites.set([
+        noPermission(this.guild.roles.everyone.id),
+        readonlyPermission(audienceRole.id),
+        readonlyPermission(playersRole.id),
+      ]);
+    } else if (_scene === "延長投票") {
+      const txtVote = readFileSync(file("延長投票.txt"))
+        .toString()
+        .replace("@GM", `<@${config.gmUserId}>`);
+      await Promise.all(
+        this.characterNames.map(async (chara) => {
+          const ch = await this.getTextChannel(chara);
+          return ch.send({ content: txtVote });
+        })
+      );
+    } else if (_scene === "投票") {
+      const txtVote = readFileSync(file("投票.txt"))
+        .toString()
+        .replace("@GM", `<@${config.gmUserId}>`);
+      await Promise.all(
+        this.characterNames.map(async (chara) => {
+          const ch = await this.getTextChannel(chara);
+          return ch.send({ content: txtVote });
+        })
+      );
+    } else if (_scene === "エンディング") {
+      const chEnding = await this.getTextChannel("エンディング");
+      await chEnding.permissionOverwrites.set([
+        noPermission(this.guild.roles.everyone.id),
+        readonlyPermission(audienceRole.id),
+        readonlyPermission(playersRole.id),
+      ]);
+    } else if (_scene === "解説・ヘンリ二世") {
+      const ch = await this.getTextChannel("ヘンリ二世");
+      await ch.permissionOverwrites.set([
+        noPermission(this.guild.roles.everyone.id),
+        readonlyPermission(audienceRole.id),
+        writerPermission(playersRole.id),
+      ]);
+    } else if (_scene === "解説・ジャック王子") {
+      const ch = await this.getTextChannel("ジャック王子");
+      await ch.permissionOverwrites.set([
+        noPermission(this.guild.roles.everyone.id),
+        readonlyPermission(audienceRole.id),
+        writerPermission(playersRole.id),
+      ]);
+    } else if (_scene === "解説・騎士ハリス") {
+      const ch = await this.getTextChannel("騎士ハリス");
+      await ch.permissionOverwrites.set([
+        noPermission(this.guild.roles.everyone.id),
+        readonlyPermission(audienceRole.id),
+        writerPermission(playersRole.id),
+      ]);
+    } else if (_scene === "解説・執事トム") {
+      const ch = await this.getTextChannel("執事トム");
+      await ch.permissionOverwrites.set([
+        noPermission(this.guild.roles.everyone.id),
+        readonlyPermission(audienceRole.id),
+        writerPermission(playersRole.id),
+      ]);
+    } else if (_scene === "解説・女中メアリ") {
+      const ch = await this.getTextChannel("女中メアリ");
+      await ch.permissionOverwrites.set([
+        noPermission(this.guild.roles.everyone.id),
+        readonlyPermission(audienceRole.id),
+        writerPermission(playersRole.id),
+      ]);
+    } else if (_scene === "解説") {
+      await Promise.all(
+        playersRole.members.map(async (user) => {
+          return user.roles.add(audienceRole);
+        })
+      );
     }
   }
 }
